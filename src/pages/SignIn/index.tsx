1250aa661fb5a3865a6c277fec40284c1ab09945
Miguel Dias <m3k3r1@gmail.com>
2021-01-27 13:03:57 +0000

    isolating button and input in component

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { FiLogIn } from 'react-icons/fi';
+import { FiLogIn, FiMail, FiLock } from 'react-icons/fi';
 import { Background, Container, Content } from './styles';
 import logoImg from '../../assets/logo.svg';
+import Input from '../../components/Input';
+import Button from '../../components/Button';
 
 const SignIn: React.FC = () => (
   <Container>
@@ -9,9 +11,14 @@ const SignIn: React.FC = () => (
       <img src={logoImg} alt="GoBarber" />
       <form>
         <h1>Login</h1>
-        <input placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit">Login</button>
+        <Input name="email" icon={FiMail} placeholder="Email" />
+        <Input
+          name="password"
+          icon={FiLock}
+          type="password"
+          placeholder="Password"
+        />
+        <Button type="submit">Login</Button>
         <a href="lkdfjg">Forgot Password</a>
       </form>
       <a href="lkdfjg">
